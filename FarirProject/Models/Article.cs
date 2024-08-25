e3c0a3979fec5ec17de6d47677e182fad9a4d958
hadis rastegar <h.rastegar@autoshenas.com>
2024-08-25 22:00:17 +0330

    Init Project :)

--- FarirProject/Models/Article.cs
+++ FarirProject/Models/Article.cs
@@ -1,21 +1,21 @@
-﻿namespace FarirProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarirProject.Models
 {
 
     public class Article
     {
+
+        [Display(Name = "شماره ی یکتا")]
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string Author { get; set; }
+        [Display(Name = "عنوان")]
+        public string? Title { get; set; }
+        [Display(Name = "محتوا")]
+        public string? Content { get; set; }
+        [Display(Name = "نویسنده")]
+        public string? Author { get; set; }
+        [Display(Name = "تاریخ انتشار")]
         public DateTime PublishedDate { get; set; }
     }
 }
 
-
-
-
-
-public class QuizDto : Quiz
-{
-    public bool IsCorrect { get; set; }
-}
--- FarirProject/Models/Quiz.cs
+++ FarirProject/Models/Quiz.cs
@@ -1,11 +1,18 @@
-﻿namespace FarirProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarirProject.Models
 {
     public class Quiz
     {
+        [Display(Name = "تاریخ انتشار")]
         public int Id { get; set; }
+        [Display(Name = "شماره ی مقاله")]
         public int ArticleId { get; set; }
+        [Display(Name = "سوال")]
         public string Question { get; set; }
+        [Display(Name = "گزینه‌ها")]
         public string Options { get; set; }
+        [Display(Name = "پاسخ صحیح")]
         public string CorrectAnswer { get; set; }
     }
 }
